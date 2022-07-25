9bba210c47bf0d2de50d1c9841da59154504f12b
flipott <ottphillipa@gmail.com>
2022-07-25 14:59:30 -0400

    Create displayEdit function that shows a filled form of selected object

--- src/displayController.js
+++ src/displayController.js
@@ -1,8 +1,11 @@
+import { handleTask } from "./taskController";
+
 //Initialize DOM
 const addModal = document.getElementById("add-item");
 const closeModal = document.getElementById("modal-close");
+const closeEditModal = document.getElementById("edit-close");
 
-//Show or hide modal depending on button click
+//Show or hide modals depending on button click
 let modalControl = (function() {
 
     addModal.addEventListener("click", function(){
@@ -13,6 +16,10 @@ let modalControl = (function() {
         hideModal();
     });
 
+    closeEditModal.addEventListener("click", function() {
+        hideEditModal();
+    });
+
     function showModal() {
         document.querySelector(".modal").style.display = "flex";
     }
@@ -21,8 +28,91 @@ let modalControl = (function() {
         document.querySelector(".modal").style.display = "none";
     }
 
-    return { showModal, hideModal }
+    function showEditModal() {
+        document.querySelector(".edit-modal").style.display = "flex";
+    }
+
+    function hideEditModal() {
+        document.querySelector(".edit-modal").style.display = "none";
+    }
+
+    return { showModal, hideModal, showEditModal, hideEditModal }
 })();
 
+//Fills out page form for editing a to-do item
+function displayEdit(editIndex) {
+    
+    modalControl.showEditModal();
 
-export { modalControl }
+    let priority = handleTask.itemArray[editIndex].priority;
+    document.getElementById("edit-priority").value = priority;
+
+    let title = handleTask.itemArray[editIndex].title;
+    document.getElementById("edit-title").value = title;
+
+    let description = handleTask.itemArray[editIndex].description;
+    document.getElementById("edit-description").value = description;
+
+    let dueDate = handleTask.itemArray[editIndex].dueDate;
+    document.getElementById("edit-date").value = dueDate;
+
+    let category = handleTask.itemArray[editIndex].category;
+    document.getElementById("edit-category").value = category;
+
+}
+
+//Populate tasks on page
+function displayTask() {
+
+    //Remove currently displayed data
+    while (document.querySelector("tbody").childNodes.length > 1) {
+        document.querySelector("tbody").removeChild(document.querySelector("tbody").lastChild);
+    }
+
+    let currentArray = handleTask.itemArray;
+    
+    //Display new data
+    for (let i=0;i<currentArray.length;i++) {
+        let priority = currentArray[i].priority;
+        let title = currentArray[i].title;
+        let description = currentArray[i].description;
+        let dueDate = currentArray[i].dueDate;
+        let category = currentArray[i].category;
+
+        let newRow = document.createElement("tr");
+        newRow.setAttribute("data-set", i);
+
+        let newInputData = document.createElement("td");
+        newInputData.innerHTML = '<input type="checkbox" value="test" checked="unchecked">'
+        newRow.appendChild(newInputData);
+
+        let newPriorityData = document.createElement("td");
+        newPriorityData.innerHTML = '<p class=' + priority + '>⚫</p>'
+        newRow.appendChild(newPriorityData);
+
+        let newTitleData = document.createElement("td");
+        newTitleData.innerHTML = title;
+        newRow.appendChild(newTitleData);
+
+        let newDescriptionData = document.createElement("td");
+        newDescriptionData.innerHTML = description;
+        newRow.appendChild(newDescriptionData);
+
+        let newDateData = document.createElement("td");
+        newDateData.innerHTML = dueDate;
+        newRow.appendChild(newDateData);
+
+        let newCategoryData = document.createElement("td");
+        newCategoryData.innerHTML = category;
+        newRow.appendChild(newCategoryData);
+
+        let newEditBtn = document.createElement("td");
+        newEditBtn.innerHTML = '<button class="edit-btn">✎</button>';
+        newRow.appendChild(newEditBtn);
+
+        document.querySelector("tbody").appendChild(newRow);
+    }
+}
+
+
+export { modalControl, displayTask, displayEdit }
