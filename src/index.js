9305b105ee9cc70c3a2d9218da7894869ad7f0d9
flipott <ottphillipa@gmail.com>
2022-07-25 14:58:59 -0400

    Add event listeners and routing for edit and delete functions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import { ca } from "date-fns/locale";
-import { modalControl } from "./displayController";
+import { modalControl, displayTask, displayEdit } from "./displayController";
 import { handleTask } from "./taskController";
 
 const submitModal = document.getElementById("modal-submit");
 let newItem;
+let editIndex;
 
-//Form responses
-
-
+//Fire DOM and task events when a new to-do item is submitted
 submitModal.addEventListener("click", function() {
 
     let priority = document.getElementById("priority").value;
@@ -16,9 +15,34 @@ submitModal.addEventListener("click", function() {
     let dueDate = document.getElementById("due-date").value;
     let category = document.getElementById("category").value; 
 
-
     handleTask.addItem(handleTask.itemFactory(priority, title, description, dueDate, category));
     modalControl.hideModal();
+
+    displayTask();
     document.getElementById("modal-form").reset();
 });
 
+document.addEventListener("click", function(e) {
+    if(e.target && e.target.className === "edit-btn") {
+        editIndex = e.target.parentElement.parentElement.getAttribute("data-set")
+        displayEdit(editIndex);
+    };
+});
+
+document.getElementById("edit-submit").addEventListener("click", function() {
+    let newPriority = document.getElementById("edit-priority").value;
+    let newTitle = document.getElementById("edit-title").value;
+    let newDescription = document.getElementById("edit-description").value;
+    let newDueDate = document.getElementById("edit-date").value;
+    let newCategory = document.getElementById("edit-category").value; 
+
+    handleTask.editItem(editIndex, newPriority, newTitle, newDescription, newDueDate, newCategory);
+    modalControl.hideEditModal();
+    displayTask();
+})
+
+document.getElementById("delete-item").addEventListener("click", function() {
+    handleTask.deleteItem(editIndex);
+    modalControl.hideEditModal();
+    displayTask();
+})
